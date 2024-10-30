cc66699863679e2788671059478c5cc7d4ff2653
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-10-30 10:58:34 -0300

    add custom query url repository

--- src/main/java/com/example/urlshortener/app/repository/UrlRepository.java
+++ src/main/java/com/example/urlshortener/app/repository/UrlRepository.java
@@ -1,9 +1,15 @@
 package com.example.urlshortener.app.repository;
 
+
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.example.urlshortener.app.entities.Url;
 
-public interface UrlRepository extends MongoRepository<Url, String >{
 
+public interface UrlRepository extends MongoRepository<Url, String> {
+    @Query( value = "{ 'shortUrl': ?0 }", fields = "{ 'longUrl': 1, '_id':0 }")
+    Optional<Url> findByShortUrl(String shortUrl);
 }
