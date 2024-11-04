f94c23ed922336580082e8cc779c254cf33b7922
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-11-04 19:08:06 -0300

    add a repository to Url entity

--- src/main/java/com/example/urlshortener/app/repository/UrlRepository.java
+++ src/main/java/com/example/urlshortener/app/repository/UrlRepository.java
@@ -5,11 +5,20 @@ import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 
 import com.example.urlshortener.app.entities.Url;
 
 
 public interface UrlRepository extends MongoRepository<Url, String> {
+    
     @Query( value = "{ 'shortUrl': ?0 }", fields = "{ 'longUrl': 1, '_id':0 }")
     Optional<Url> findByShortUrl(String shortUrl);
+
+    @Query( value = "{'shortUrl': ?0 }", fields = "{ 'clickCount': 1, '_id':0 }")
+    Optional<Url> findClickCountByShortUrl(String shortUrl);
+
+    @Query(value = "{ 'shortUrl': ?0 }")
+    @Update("{ '$inc': { 'clickCount':1}}")
+    void incrementClickCount(String shortUrl);
 }
