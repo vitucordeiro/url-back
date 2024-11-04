3c629899b85823aedc0c7f48118f982f19617562
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-11-04 19:43:07 -0300

    fix configurations from SecurityConfig

--- src/main/java/com/example/urlshortener/app/config/SecurityConfig.java
+++ src/main/java/com/example/urlshortener/app/config/SecurityConfig.java
@@ -11,14 +11,7 @@ public class SecurityConfig {
     @SuppressWarnings("removal")
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()  // Desabilita CSRF para facilitar o acesso ao H2
-            .headers().frameOptions().disable()  // Permite o uso de frames (necessário para o H2)
-            .and()
-            .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/h2-console/**").permitAll()  // Permite acesso ao H2 Console
-                .anyRequest().authenticated()  // Todas as outras requisições precisam de autenticação
-            );
+        http.csrf().disable();
         return http.build();
     }
 }
