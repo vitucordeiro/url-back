716eb63240552b972f5efb887f874c13d75d1b4f
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-11-12 16:01:21 -0300

    refactor: add a constant that return a string legth for method

--- src/main/java/com/example/urlshortener/app/services/UrlService.java
+++ src/main/java/com/example/urlshortener/app/services/UrlService.java
@@ -9,6 +9,7 @@ import com.example.urlshortener.app.dto.UrlDTO;
 import com.example.urlshortener.app.entities.Url;
 import com.example.urlshortener.app.repository.UrlRepository;
 import com.example.urlshortener.app.services.exceptions.ResourceNotFoundException;
+import com.example.urlshortener.app.utils.Constants;
 import com.example.urlshortener.app.utils.UrlShortenerUtil;
 
 import lombok.extern.slf4j.Slf4j;
@@ -49,7 +50,7 @@ public class UrlService {
     public String createShortenerUrl(UrlDTO entityDTO){
         Url url = new Url(entityDTO);
         UrlShortenerUtil util = new UrlShortenerUtil();
-        String shortUrl = util.RandomStringFactory(10);
+        String shortUrl = util.RandomStringFactory(Constants.MAX_LENGTH_RANDOM_STRING);
         url.setShortUrl(shortUrl);
         
         url = repository.save(url);
