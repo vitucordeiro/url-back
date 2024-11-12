c77a385b6292108d872910b5e2533a87caa9ffb7
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-11-12 16:02:13 -0300

    add: a util class to returns some values for some methods

---
+++ src/main/java/com/example/urlshortener/app/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.example.urlshortener.app.utils;
+
+public final class Constants {
+    public static final int MAX_LENGTH_RANDOM_STRING = 22;
+
+    // Private constructor to prevent instantiation
+    private Constants() {
+        throw new UnsupportedOperationException("This is a constants class and cannot be instantiated");
+    }
+}
